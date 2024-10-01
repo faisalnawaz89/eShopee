a227569acfceaf337895b3a1a115e7ad29908dd0
Faisal Nawaz <isfaisalnawaz@gmail.com>
2024-10-01 15:18:57 +0530

    add spiner changes

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -5,21 +5,27 @@ const Item = (props) => {
   const {loading} = useContext(ShopContext)
 
   if (loading) {
-    return <div className="spinner"></div>
+
+    return <div className='spinner-container'><div className="spinner"></div></div> 
+
+  }else{
+
+    return (
+      <div className='product-item'>
+          <Link to={`/product/${props.productId}`}>
+            <img src={props.productImage} alt=''/>
+          </Link>
+          <div className='product-name'>{props.productName}</div>
+          <div className='product-price'>
+              <span className='new-prce'>₹{props.productNewPrice}</span>
+              <span className='old-price'>₹{props.productOldPrice}</span>
+          </div>
+      </div>
+    )
+    
   }
 
-  return (
-    <div className='product-item'>
-        <Link to={`/product/${props.productId}`}>
-          <img src={props.productImage} alt=''/>
-        </Link>
-        <div className='product-name'>{props.productName}</div>
-        <div className='product-price'>
-            <span className='new-prce'>₹{props.productNewPrice}</span>
-            <span className='old-price'>₹{props.productOldPrice}</span>
-        </div>
-    </div>
-  )
+  
 }
 
 export default Item
